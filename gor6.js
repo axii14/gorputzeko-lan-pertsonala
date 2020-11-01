d4155c070689f288a3119273ad9bd8f047f70a8c
axii14 <64701220+axii14@users.noreply.github.com>
2020-11-01 07:05:50 +0100

    Create gor6.js

---
+++ gor6.js
@@ -0,0 +1,19 @@
+var i = 0;
+$(document).ready(function(){
+  var altuera = $("#zatixa").height();    
+  $("#a").css("margin-top",altuera+"px");
+  });
+
+$( "#zatixa" ).scroll(function() {
+  if ($('.line').visible(true,true)) {}
+  if ($('.agertu').visible(false,true)) {
+  $('.agertu').addClass("animated slideInLeft visible");
+  }
+
+if($("").visible(true)&& i == 0){
+  var denporia = new TimelineMax();
+  denporia.add([
+  TweenLite.to(paths.eq(0), 4, {
+  strokeDashoffset: 0,
+})]);}});
+    
